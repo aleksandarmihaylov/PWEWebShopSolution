8cb646eb0886d4d3cfc34476a686ab02300c2cbc
aleksandarmihaylov <aleksandaryanev95@gmail.com>
2017-09-21 10:47:27 +0200

    Creating the save method for the category class + seting up a database and database connection

--- PWEWebShop/DAL/CategoryRepository.cs
+++ PWEWebShop/DAL/CategoryRepository.cs
@@ -1,6 +1,7 @@
 ﻿using PWEWebShop.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,54 @@ namespace PWEWebShop.DAL
             allCategories = new List<Category>();
         }
 
+        public void SaveCategory(Category category)
+        {
+            //save the category to the database here
+            //1st step. set up and configure connection ADO.NET
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "Server=DESKTOP-PVFC580\SQLEXPRESS;Database=PWEWebShop;Integrated Secturity=SSPI;";
+            try
+            {
+                //1.5 step. open the connection
+                connection.Open();
+                //2nd step. set up and write sql command
+                SqlCommand mySqlCommand = connection.CreateCommand();
+                mySqlCommand.CommandText = "insert into Category(Name, Description) values('" + category.Name + "', '" + category.Description + "')";
+                //3rd step. execute the sql command
+                mySqlCommand.ExecuteNonQuery();
+                //4th step. close the connection
+                connection.Close();
+            }
+            catch(Exception ex)
+            {
+                connection.Close();
+            }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            //update the category here
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            //delete the category from the database here
+        }
+
+        public Category LoadCategory(int id)
+        {
+            //return null, I will get back later on
+            //this method is for loading a single catefgory
+            // like for the edit category page in our real webshop
+            return null;
+        }
+
+        public List<Category> LoadAllCategories()
+        {
+            //This is for  loading all the categories
+            //shoud i sor this alphabetically?
+            return null;
+        }
         public void Save(Category category)
         {
             allCategories.Add(category);
